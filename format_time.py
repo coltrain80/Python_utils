42d951869b0450fb9701518c61c647c4f87096f0
Anthony L Fillmore <coltrain8180@gmail.com>
2024-06-15 21:26:54 -0500

    Create format_time.py

---
+++ format_time.py
@@ -0,0 +1,10 @@
+def format_time(seconds):
+    if seconds < 400:
+        s = float(seconds)
+        return "%.1f seconds" % (s,)
+    elif seconds < 4000:
+        m = seconds / 60.0
+        return "%.2f minutes" % (m,)
+    else:
+        h = seconds / 3600.0
+        return "%.2f hours" % (h,)
